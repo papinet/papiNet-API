e5dd66befd2ee8c845aa8e0a6edca4c953a0f894
Patrice Krakow <patrice.krakow@gmail.com>
2021-05-05 15:13:50 +0200

    papiNet API Mock Server, refactoring (1)

--- 1.1.0/mock/app.js
+++ 1.1.0/mock/app.js
@@ -24,7 +24,41 @@ const sessionsIndexedByToken = []
 
 const scenarios = [
   {
-    id: "A",
+    method: "GET",
+    domain:"papinet.papinet.io",
+    path: "/orders",
+    useCase: "order-status-use-case",
+    name: "_",
+    firstStep: 0,
+    lastStep: 0,
+    previousStep: -1
+  },
+  {
+    method: "GET",
+    domain:"papinet.papinet.io",
+    path: "/orders/c51d8903-01d1-485c-96ce-51a9be192207",
+    useCase: "order-status-use-case",
+    name: "A",
+    firstStep: 1,
+    lastStep: 1,
+    previousStep: 0
+  },
+  {
+    method: "GET",
+    domain:"papinet.road.papinet.io",
+    path: "/shipments",
+    useCase: "shipment-use-case",
+    name: "A",
+    firstStep: 2,
+    lastStep: 2,
+    previousStep: 0
+  },
+  {
+    method: "GET",
+    domain:"papinet.road.papinet.io",
+    path: "/shipments/c51d8903-01d1-485c-96ce-51a9be192207",
+    useCase: "shipment-use-case",
+    name: "A",
     shipmentId: "c51d8903-01d1-485c-96ce-51a9be192207",
     party: "Road",
     firstStep: 3,
@@ -32,7 +66,9 @@ const scenarios = [
     previousStep: 0
   },
   {
-    id: "B",
+    path: "/shipments/3a9108d5-f7f0-42ae-9a29-eb302bdb8ede",
+    useCase: "shipment-use-case",
+    name: "B",
     shipmentId: "3a9108d5-f7f0-42ae-9a29-eb302bdb8ede",
     party: "Fast",
     firstStep: 3,
@@ -40,7 +76,9 @@ const scenarios = [
     previousStep: 0
   },
   {
-    id: "C",
+    path: "/shipments/d4fd1f2c-642f-4df8-a7b3-139cf9d63d17",
+    useCase: "shipment-use-case",
+    name: "C",
     shipmentId: "d4fd1f2c-642f-4df8-a7b3-139cf9d63d17",
     party: "Pulp",
     firstStep: 3,
@@ -49,9 +87,20 @@ const scenarios = [
   }
 ]
 const scenariosIndexedByParty = [
+  "GET|papinet.papinet.io|/orders",
+  "GET|papinet.papinet.io|/orders/c51d8903-01d1-485c-96ce-51a9be192207",
+  "GET|papinet.road.papinet.io|/shipments",
   "Road",
   "Fast",
   "Pulp"
+]
+const scenariosIndexedByAPIEndpoint = [
+  "GET|papinet.papinet.io|/orders",
+  "GET|papinet.papinet.io|/orders/c51d8903-01d1-485c-96ce-51a9be192207",
+  "GET|papinet.road.papinet.io|/shipments",
+  "GET|papinet.road.papinet.io|/shipments/c51d8903-01d1-485c-96ce-51a9be192207",
+  "GET|papinet.road.papinet.io|/shipments/3a9108d5-f7f0-42ae-9a29-eb302bdb8ede",
+  "GET|papinet.road.papinet.io|/shipments/d4fd1f2c-642f-4df8-a7b3-139cf9d63d17"
 ]
 
 // post /tokens
@@ -59,8 +108,12 @@ app.post('/tokens', (req, res) => {
   const traceId = short.uuid()
   console.log(`[INFO] [${traceId}] post /tokens [${Date.now()}] `)
   const id = short.uuid()
+  // Depreacted:
   const party = req.header('X-papiNet-Party')
   console.log(`[INFO] [${traceId}]   party = ${party} [${Date.now()}]`)
+  // Replaced By:
+  const domain = req.header('X-papiNet-Domain')
+  console.log(`[INFO] [${traceId}]   domain = ${domain} [${Date.now()}]`)
 
   const scenarioPos = scenariosIndexedByParty.indexOf(party)
   console.log(`[INFO] [${traceId}]   scenarioPos: ${scenarioPos}`)
@@ -71,8 +124,10 @@ app.post('/tokens', (req, res) => {
   const session = {
     id: id,
     token: id,
+    domain: domain,
     party: party,
-    scenario: sessionScenario
+    scenario: sessionScenario,
+    scenarioActivated: false
   }
   sessions.push(session)
   sessionsIndexedByToken.push(session.token)
@@ -86,11 +141,121 @@ app.get('/debug', (req, res) => {
   return res.status(200).send(sessions);
 })
 
+// get /orders
+app.get('/orders', (req, res) => {
+  const traceId = short.uuid()
+  console.log(`[INFO] [${traceId}] get /orders [${Date.now()}]`)
+
+  const method = 'GET'
+  console.log(`[INFO] [${traceId}]   method: ${method}`)
+  const domain = req.header('X-papiNet-Domain')
+  console.log(`[INFO] [${traceId}]   domain: ${domain}`)
+  const path = '/orders'
+  console.log(`[INFO] [${traceId}]   path: ${path}`)
+
+  const authorization = req.headers.authorization
+  console.log(`[INFO] [${traceId}]   authorization: ${authorization} [${Date.now()}]`)
+
+  if (authorization === undefined || !authorization.startsWith("Bearer ")) {
+    return res.status(401).json({
+      error: {
+        code: "ERR-04",
+        message: "Cannot get a bearer token :-("
+      }
+    })
+  }
+
+  const token = authorization.substring(7, authorization.length)
+  console.log(`[INFO] [${traceId}]   token: ${token}`)
+
+  const sessionPos = sessionsIndexedByToken.indexOf(token)
+  console.log(`[INFO] [${traceId}]   sessionPos: ${sessionPos}`)
+
+  if (sessionPos === -1) {
+    return res.status(401).json({
+      error: {
+        code: "ERR-05",
+        message: "Cannot retrieve the session based on the Bearer token :-("
+      }
+    })
+  }
+
+  const sessionDomain = sessions[sessionPos].domain
+  console.log(`[INFO] [${traceId}]   sessionDomain: ${sessionDomain}`)
+
+  if (sessionDomain !== domain) {
+    return res.status(401).json({
+      error: {
+        code: "ERR-06",
+        message: "The 'X-papiNet-Domain' does not correpsond to the session's party :("
+      }
+    })
+  }
+
+  const apiEnpoint = `${method}|${domain}|${path}`
+  console.log(`[INFO] [${traceId}]   apiEnpoint: ${apiEnpoint}`)
+
+  const scenarioPos = scenariosIndexedByAPIEndpoint.indexOf(apiEnpoint)
+  console.log(`[INFO] [${traceId}]   scenarioPos: ${scenarioPos}`)
+
+  // This is the (ONLY) good way to make a genuine COPY!!!
+  const sessionScenario = JSON.parse(JSON.stringify(scenarios[scenarioPos]))
+
+  if (sessions[sessionPos].scenarioActivated === false) {
+    sessions[sessionPos].scenario = sessionScenario
+    sessions[sessionPos].scenarioActivated = true
+  }
+
+  const sessionScenarioName = sessions[sessionPos].scenario.name
+  console.log(`[INFO] [${traceId}]   sessionScenarioName: ${sessionScenarioName}`)
+
+  const sessionScenarioUseCase = sessions[sessionPos].scenario.useCase
+  console.log(`[INFO] [${traceId}]   sessionScenarioUseCase: ${sessionScenarioUseCase}`)
+
+  const firstStep = sessions[sessionPos].scenario.firstStep
+  console.log(`[INFO] [${traceId}]   firstStep: ${firstStep}`)
+  const lastStep = sessions[sessionPos].scenario.lastStep
+  console.log(`[INFO] [${traceId}]   lastStep: ${lastStep}`)
+  const previousStep = sessions[sessionPos].scenario.previousStep
+  console.log(`[INFO] [${traceId}]   previousStep: ${previousStep}`)
+
+  let currentStep = -1
+  if (previousStep < firstStep) {
+    currentStep = firstStep
+  }
+  else if (previousStep < lastStep) {
+    currentStep = previousStep + 1
+  }
+  else if (previousStep >= lastStep) {
+    currentStep = lastStep
+  }
+  console.log(`[INFO] [${traceId}]   currentStep: ${currentStep}`)
+
+  sessions[sessionPos].scenario.previousStep = currentStep
+
+  const sourceFileName = `./samples/${sessionScenarioUseCase}.${sessionScenarioName}.step-${currentStep}.json`;
+  console.log(`[INFO] [${traceId}]   sourceFileName: ${sourceFileName}`);
+
+  let source = {}
+  try {
+    source = jsonfile.readFileSync(sourceFileName) // MUST be changed to async
+  } catch (err) {
+    return res.status(404).json({
+      error: {
+        code: "ERR-06",
+        message: `Cannot find the file \'${sourceFileName}\'.`
+      }
+    })
+  }
+
+  return res.status(200).send(source)
+})
+
 // get /shipments
 app.get('/shipments', (req, res) => {
-  const traceId = short.uuid();
+  const traceId = short.uuid()
   console.log(`[INFO] [${traceId}] get /shipments [${Date.now()}]`)
-  const authorization = req.headers.authorization;
+  const authorization = req.headers.authorization
   console.log(`[INFO] [${traceId}]   authorization: ${authorization} [${Date.now()}]`)
 
   if (authorization === undefined || !authorization.startsWith("Bearer ")) {
@@ -132,10 +297,10 @@ app.get('/shipments', (req, res) => {
     })
   }
 
-  const scenarioId = sessions[sessionPos].scenario.id
-  console.log(`[INFO] [${traceId}]   scenarioId: ${scenarioId}`)
+  const sessionScenarioName = sessions[sessionPos].scenario.name
+  console.log(`[INFO] [${traceId}]   sessionScenarioName: ${sessionScenarioName}`)
 
-  const sourceFileName = `./samples/shipment-use-case.${scenarioId}.step-2.json`;
+  const sourceFileName = `./samples/shipment-use-case.${sessionScenarioName}.step-2.json`;
   console.log(`[INFO] [${traceId}]   sourceFileName: ${sourceFileName}`);
 
   let source = {}
@@ -200,8 +365,8 @@ app.get('/shipments/:shipmentId', (req, res) => {
     })
   }
 
-  const scenarioId = sessions[sessionPos].scenario.id
-  console.log(`[INFO] [${traceId}]   scenarioId: ${scenarioId}`)
+  const sessionScenarioName = sessions[sessionPos].scenario.name
+  console.log(`[INFO] [${traceId}]   sessionScenarioName: ${sessionScenarioName}`)
 
   const sessionShipmentId = sessions[sessionPos].scenario.shipmentId
   console.log(`[INFO] [${traceId}]   sessionShipmentId: ${sessionShipmentId}`)
@@ -236,7 +401,7 @@ app.get('/shipments/:shipmentId', (req, res) => {
 
   sessions[sessionPos].scenario.previousStep = currentStep
 
-  const sourceFileName = `./samples/shipment-use-case.${scenarioId}.step-${currentStep}.json`;
+  const sourceFileName = `./samples/shipment-use-case.${sessionScenarioName}.step-${currentStep}.json`;
   console.log(`[INFO] [${traceId}]   sourceFileName: ${sourceFileName}`);
 
   let source = {}
@@ -256,3 +421,7 @@ app.get('/shipments/:shipmentId', (req, res) => {
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
+
+function handle(method, domain, path) {
+  return {}
+}
