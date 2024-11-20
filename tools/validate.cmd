0000fd1e595ed641708ad71a30f7eb89a2b52258
Patrice Krakow <patrice.krakow@gmail.com>
2024-11-20 15:01:41 +0100

    FIX issue #190: `ajv` was not using the `unevaluatedProperties: false` because of the missing `--spec=draft2019` option

--- tools/validate.cmd
+++ tools/validate.cmd
@@ -11,7 +11,7 @@ PowerShell -Command "(Get-Content ListOfSupplierOrders.schema.yaml) -replace '#/
 
 yq -o=json "." ListOfSupplierOrders.schema.yaml > ListOfSupplierOrders.schema.json
 
-call ajv validate -s ListOfSupplierOrders.schema.json -d "..\3.0.0\mock\GW01&BW01.get-supplier-orders.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s ListOfSupplierOrders.schema.json -d "..\3.0.0\mock\GW01&BW01.get-supplier-orders.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -26,7 +26,7 @@ PowerShell -Command "(Get-Content SupplierOrderById.schema.yaml) -replace '#/com
 
 yq -o=json "." SupplierOrderById.schema.yaml > SupplierOrderById.schema.json
 
-call ajv validate -s SupplierOrderById.schema.json -d "..\3.0.0\mock\GW02&BW02.get-supplier-orders-supplierOrderId.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s SupplierOrderById.schema.json -d "..\3.0.0\mock\GW02&BW02.get-supplier-orders-supplierOrderId.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -41,7 +41,7 @@ PowerShell -Command "(Get-Content ListOfLogisticsDeliveryNotes.schema.yaml) -rep
 
 yq -o=json "." ListOfLogisticsDeliveryNotes.schema.yaml > ListOfLogisticsDeliveryNotes.schema.json
 
-call ajv validate -s ListOfLogisticsDeliveryNotes.schema.json -d "..\3.0.0\mock\GW03&BW03.get-logistic-delivery-notes.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s ListOfLogisticsDeliveryNotes.schema.json -d "..\3.0.0\mock\GW03&BW03.get-logistic-delivery-notes.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -56,7 +56,7 @@ PowerShell -Command "(Get-Content LogisticsDeliveryNoteById.schema.yaml) -replac
 
 yq -o=json "." LogisticsDeliveryNoteById.schema.yaml > LogisticsDeliveryNoteById.schema.json
 
-call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\GW04&BW04.get-logistic-delivery-notes-logisticDeliveryNoteId.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\GW04&BW04.get-logistic-delivery-notes-logisticDeliveryNoteId.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -71,14 +71,14 @@ PowerShell -Command "(Get-Content LogisticsGoodsReceiptById.schema.yaml) -replac
 
 yq -o=json "." LogisticsGoodsReceiptById.schema.yaml > LogisticsGoodsReceiptById.schema.json
 
-call ajv validate -s LogisticsGoodsReceiptById.schema.json -d "..\3.0.0\mock\GW05&BW05.post-logistic-goods-receipts.request.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsGoodsReceiptById.schema.json -d "..\3.0.0\mock\GW05&BW05.post-logistic-goods-receipts.request.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
 rem -------------------------------------------------------------------
 rem BW06.put-logistic-goods-receipts-logisticGoodReceiptId.request.json ~ LogisticsGoodsReceiptById
 
-call ajv validate -s LogisticsGoodsReceiptById.schema.json -d "..\3.0.0\mock\BW06.put-logistic-goods-receipts-logisticGoodReceiptId.request.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsGoodsReceiptById.schema.json -d "..\3.0.0\mock\BW06.put-logistic-goods-receipts-logisticGoodReceiptId.request.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -93,7 +93,7 @@ PowerShell -Command "(Get-Content ListOfDeliveryInstructions.schema.yaml) -repla
 
 yq -o=json "." ListOfDeliveryInstructions.schema.yaml > ListOfDeliveryInstructions.schema.json
 
-call ajv validate -s ListOfDeliveryInstructions.schema.json -d "..\3.0.0\mock\GW06&BW07.get-delivery-instructions.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s ListOfDeliveryInstructions.schema.json -d "..\3.0.0\mock\GW06&BW07.get-delivery-instructions.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
@@ -108,34 +108,34 @@ PowerShell -Command "(Get-Content DeliveryInstructionById.schema.yaml) -replace 
 
 yq -o=json "." DeliveryInstructionById.schema.yaml > DeliveryInstructionById.schema.json
 
-call ajv validate -s DeliveryInstructionById.schema.json -d "..\3.0.0\mock\GW07.get-delivery-instructions-deliveryInstructionId.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s DeliveryInstructionById.schema.json -d "..\3.0.0\mock\GW07.get-delivery-instructions-deliveryInstructionId.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
 rem -------------------------------------------------------------------
 rem BW08.get-delivery-instructions-deliveryInstructionId.response.json ~ DeliveryInstructionById
 
-call ajv validate -s DeliveryInstructionById.schema.json -d "..\3.0.0\mock\BW08.get-delivery-instructions-deliveryInstructionId.response.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s DeliveryInstructionById.schema.json -d "..\3.0.0\mock\BW08.get-delivery-instructions-deliveryInstructionId.response.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
 rem -------------------------------------------------------------------
 rem GW08.post-logistic-delivery-notes.request.json ~ DeliveryInstructionById
 
-call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\GW08.post-logistic-delivery-notes.request.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\GW08.post-logistic-delivery-notes.request.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
 rem -------------------------------------------------------------------
 rem BW09.post-logistic-delivery-notes.request.json ~ DeliveryInstructionById
 
-call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\BW09.post-logistic-delivery-notes.request.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\BW09.post-logistic-delivery-notes.request.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
 
 rem -------------------------------------------------------------------
 rem BW10.put-logistic-delivery-notes-logisticDeliveryNoteId.request.json ~ DeliveryInstructionById
 
-call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\BW10.put-logistic-delivery-notes-logisticDeliveryNoteId.request.json" --strict=false -c ajv-formats --verbose
+call ajv validate -s LogisticsDeliveryNoteById.schema.json -d "..\3.0.0\mock\BW10.put-logistic-delivery-notes-logisticDeliveryNoteId.request.json" --strict=false -c ajv-formats --verbose --spec=draft2019
 @echo on
 @echo off
